0811d9658b6189383242c6b0a25d238d6882d07e
Volodymyr Nazaryshyn <volodimirnazarisin@gmail.com>
2023-04-14 21:34:22 +0300

    2048 ClassWork 3

--- app/src/main/java/step/learning/course/GameActivity.java
+++ app/src/main/java/step/learning/course/GameActivity.java
@@ -7,6 +7,9 @@ import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +20,7 @@ import java.util.Random;
 public class GameActivity extends AppCompatActivity {
     private final int N = 4;
     private final int[][] cells = new int[N][N]; // значения в ячейках поля
+    private final int[][] saves = new int[N][N]; // предыдущий ход
     private final TextView[][] tvCells = new TextView[N][N]; // ссылки на ячейки поля
     private final Random random = new Random();
 
@@ -24,6 +28,8 @@ public class GameActivity extends AppCompatActivity {
     private int bestScore;
     private TextView tvScore;
     private TextView tvBestScore;
+    private Animation spawnAnimation;
+    private Animation collapseAnimation;
 
     @SuppressLint("DiscouragedApi")
     @Override
@@ -35,6 +41,11 @@ public class GameActivity extends AppCompatActivity {
         tvBestScore = findViewById(R.id.game_tv_best_score);
         tvScore.setText(getString(R.string.game_score, "69.6k"));
         tvBestScore.setText(getString(R.string.game_best_score, "69.6k"));
+
+        spawnAnimation = AnimationUtils.loadAnimation(this, R.anim.cell_spawn);
+        spawnAnimation.reset();
+        collapseAnimation = AnimationUtils.loadAnimation(this, R.anim.cell_collapse);
+        collapseAnimation.reset();
 
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
@@ -53,11 +64,20 @@ public class GameActivity extends AppCompatActivity {
                         new OnSwipeTouchListener(GameActivity.this) {
                             @Override
                             public void onSwipeRight() {
-                                Toast.makeText(
-                                        GameActivity.this,
-                                        "Right",
-                                        Toast.LENGTH_SHORT)
-                                    .show();
+                                //saveField(); // [0204] -> [0024] -> no move [undo]
+                                if (canMoveRight()) {
+                                    saveField();
+                                    moveRight();
+                                    spawnCell(1);
+                                    showField();
+                                }
+                                else {
+                                    Toast.makeText(
+                                                    GameActivity.this,
+                                                    "No Right Move",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                }
                             }
                             @Override
                             public void onSwipeLeft() {
@@ -86,10 +106,11 @@ public class GameActivity extends AppCompatActivity {
                         }
                 );
 
-        newGame();
+        findViewById(R.id.game_new).setOnClickListener(this::newGame);
+        newGame(null);
     }
 
-    private void newGame() {
+    private void newGame(View view) {
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
                 cells[i][j] = 0;
@@ -100,6 +121,14 @@ public class GameActivity extends AppCompatActivity {
         showField();
     }
 
+    private boolean canMoveRight() {
+       /*
+        Д.З. Реализовать проверку возможности хода вправо (без изменения состояния поля)
+        ** реализовать ходы и проверки по другим направлениям
+         */
+        return true;
+    }
+
     @SuppressLint("DiscouragedApi")
     private void showField() {
         Resources resources = getResources();
@@ -107,6 +136,13 @@ public class GameActivity extends AppCompatActivity {
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
                 tvCells[i][j].setText(String.valueOf(cells[i][j]));
+                float textSize = resources.getDimension( // R.dimen.txt_size_game_cell_4096
+                        resources.getIdentifier(
+                        "txt_size_game_cell_" + cells[i][j],
+                        "dimen",
+                        packageName
+                ));
+                tvCells[i][j].setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     tvCells[i][j].setTextAppearance( // R.style.GameCell_16
                             resources.getIdentifier(
@@ -176,9 +212,65 @@ public class GameActivity extends AppCompatActivity {
             int y = coordinates.get(randIndex).getY();
             // ставим в ячейку 2 / 4
             cells[x][y] = random.nextInt(10) == 0 ? 4 : 2;
+            // проигрываем анимацию для появившейся ячейки
+            tvCells[x][y].startAnimation(spawnAnimation);
         }
 
         return true;
+    }
+
+    private boolean moveRight() {
+        boolean isMoved = false ;    // [0002]->[0002], [0200]->[0002], [2020]->[0022]->[0004]
+
+        for( int i = 0; i < N; ++i ) {
+            // сдвиги
+            boolean wasReplace;
+            do {
+                wasReplace = false;
+                for (int j = N - 1; j > 0; --j) {
+                    if (cells[i][j] == 0          // текущая ячейка 0
+                            && cells[i][j - 1] != 0) {    // а перед ней - не 0
+                        cells[i][j] = cells[i][j - 1];
+                        cells[i][j - 1] = 0;
+                        wasReplace = true;
+                        isMoved = true;
+                    }
+                }
+            } while (wasReplace);
+
+            // collapse
+            for (int j = N - 1; j > 0; --j) {  // [2202] -> [0222] -> [0204] -> [0024]
+                if (cells[i][j] == cells[i][j - 1] && cells[i][j] != 0) {  // соседние ячейки равны  [2222]
+                    score += cells[i][j] + cells[i][j - 1] ;   // счет = сумма всех объединенных ячеек
+                    cells[i][j] *= -2 ;  // [2224]; "-" - признак для анимации
+                    cells[i][j - 1] = 0 ;   // [2204]
+                    isMoved = true ;
+                }
+            }  // [0404]  при коллапсе может понадобиться дополнительное смещение
+            for (int j = N - 1; j > 0; --j) {
+                if (cells[i][j] == 0 && cells[i][j - 1] != 0) {
+                    cells[i][j] = cells[i][j - 1];
+                    cells[i][j - 1] = 0;
+                }
+            }
+            for (int j = N - 1; j > 0; --j) {
+                if( cells[i][j] < 0 ) {  // надо включить анимацию
+                    cells[i][j] = -cells[i][j] ;
+                    tvCells[i][j].startAnimation( collapseAnimation ) ;
+                }
+            } // [0044]
+        }
+        return isMoved ;
+    }
+    private void saveField() {
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(cells[i], 0, saves[i], 0, N);
+        }
+    }
+    private void undoMove() {
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(saves[i], 0, cells[i], 0, N);
+        }
     }
 
     private class Coord {
@@ -199,3 +291,20 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 }
+/*
+Анимации
+1. Создаем ресурсную папку res/anim (ПКМ - папка - anim)
+2. Создаем ресурсный файл cell_spawn.xml, корневой элемент alpha (см файл)
+3. Создаем переменную типа Animation spawnAnimation
+4. Загружаем анимацию
+    spawnAnimation = AnimationUtils.loadAnimation(this, R.anim.cell_spawn);
+    spawnAnimation.reset();
+5. Проигрываем анимацию (на любом View)
+    tvCells[x][y].startAnimation(spawnAnimation);
+
+Виды анимаций
+alpha - прозрачность
+rotate - вращение
+translate - перемещение
+scale - размер (масштаб)
+ */
